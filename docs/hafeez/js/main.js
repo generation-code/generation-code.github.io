eb87ed38fb999d5cd0c53eb17dbe82e034a9fd94
ericfoster9 <eric.foster@nycda.com>
2018-06-18 11:24:44 -0400

    docs commit

---
+++ docs/hafeez/js/main.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    myVar = setTimeout(showPage, 100);
+}
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
